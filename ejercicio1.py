d6ce1f7613cac49742b9effdf06086078e7fe352
ohaki <ohaki@myuax.com>
2024-02-13 16:07:13 +0100

    cambios

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,2 +1,12 @@
 print ("hola mundito")
 
+hola = "hola"
+mundo = "mundo"
+print (hola, mundo)
+
+print (hola + " " + mundo)
+
+print (hola + mundo)
+
+print (hola, mundo, sep="")
+print (hola, mundo, sep=" ")
